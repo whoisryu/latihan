478a8c17f3085f253a883e89be0f53b2c3d12f11
whoisryu <aryuskap@gmail.com>
2019-11-23 18:55:32 +0700

    bubble sort

---
+++ bubbleSort.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+var numbers = []int{8, 6, 4, 2, 0, 9}
+
+var k int
+
+func main() {
+	bubbleSort(numbers)
+}
+
+func bubbleSort(numbers []int) {
+	var N = len(numbers)
+
+	for i := 0; i < N; i++ {
+		if !sweep(numbers, i) {
+			return
+		}
+
+		fmt.Println(numbers)
+	}
+}
+
+func sweep(numbers []int, prevPasses int) bool {
+	var (
+		N           = len(numbers)
+		firstIndex  = 0
+		secondIndex = 1
+		didSwap     = false
+	)
+	for secondIndex < (N - prevPasses) {
+		var firstNumber = numbers[firstIndex]
+		var secondNumber = numbers[secondIndex]
+
+		//do swap
+		if firstNumber > secondNumber {
+			numbers[firstIndex] = secondNumber
+			numbers[secondIndex] = firstNumber
+			didSwap = true
+		}
+		firstIndex++
+		secondIndex++
+	}
+	return didSwap
+}
